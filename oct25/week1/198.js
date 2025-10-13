3d596b4a53ffe807e3275ae81a312cbed926711a
Ashis Kumar Joy <ashis263@gmail.com>
2025-10-13 12:18:18 +0600

    october week1

---
+++ oct25/week1/198.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  let totalMoney = 0;
+
+  let i = 0;
+  while(i<nums.length){
+    
+  }
+
+  return totalMoney;
+};
+
+console.log(rob([2, 7, 9, 3, 1]));
